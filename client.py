98b253dd05fcdaa8dd37023e76d9c95bc85b5eb7
Alexander Belov <git@belov.by>
2014-12-11 01:39:38 +0300

    Initial commit

---
+++ client.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import socket
+import sys
+import os
+
+if len(sys.argv) < 3:
+    print 'Usage: client.py HOST PORT'
+    sys.exit(0)
+
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
+UPLOAD_COMMAND = 'upload'
+DOWNLOAD_COMMAND = 'download'
+BUFFER_SIZE = 2
+
+def tcp_upload_file(filename):
+    try:
+        file = open(filename, "rb")
+    except Exception:
+        print 'File not found'
+        return 1
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+
+    file_length = str(os.path.getsize(filename))
+    filename_length = len(filename)
+
+    s.send(chr(len(UPLOAD_COMMAND)))
+    s.send(UPLOAD_COMMAND)
+    s.send(chr(filename_length))
+    s.send(filename)
+    s.send(chr(len(file_length)))
+    s.send(file_length)
+
+    while True:
+        msg = file.read(BUFFER_SIZE)
+        if len(msg) == 0:
+            break
+        s.send(msg)
+
+    file.close
+    s.close
+
+    return 0
+
+def tcp_download_file(filename):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+
+    s.send(chr(len(DOWNLOAD_COMMAND)))
+    s.send(DOWNLOAD_COMMAND)
+
+    filename_length = len(filename)
+    s.send(chr(filename_length))
+    s.send(filename)
+
+    file_size_length = ord(s.recv(1))
+
+    if file_size_length == 0:
+        print 'File not found'
+        return 1
+
+    file_size = int(s.recv(file_size_length, socket.MSG_WAITALL))
+    data = s.recv(file_size, socket.MSG_WAITALL)
+
+    file = open('old_' + filename, "wb")
+    file.write(data)
+
+    file.close()
+    s.close
+
+    return 0
+
+while True:
+    command = raw_input('').rstrip('\n')
+    command = str.split(command)
+    if len(command) != 2:
+        continue
+    if command[0] == 'upload':
+        tcp_upload_file(command[1])
+    elif command[0] == 'download':
+        tcp_download_file(command[1])
---
+++ server.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import socket
+import sys
+import os
+
+if len(sys.argv) < 3:
+    print 'Usage: client.py HOST PORT'
+    sys.exit(0)
+
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
+UPLOAD_COMMAND = 'upload'
+DOWNLOAD_COMMAND = 'download'
+BUFFER_SIZE = 5
+
+def tcp_upload_file(conn, filename):
+    try:
+        file = open(filename, "rb")
+    except Exception:
+        print 'File not found'
+        conn.send(chr('0'))
+        return 1
+
+    file_length = str(os.path.getsize(filename))
+
+    conn.send(chr(len(file_length)))
+    conn.send(file_length)
+
+    print file_length
+
+    while True:
+        msg = file.read(BUFFER_SIZE)
+        if len(msg) == 0:
+            break
+        conn.send(msg)
+
+    file.close
+
+    return 0
+
+def tcp_download_file(conn):
+    filename_length = ord(conn.recv(1))
+    filename = conn.recv(filename_length, socket.MSG_WAITALL)
+    file_size_length = ord(conn.recv(1))
+    file_length = int(conn.recv(file_size_length, socket.MSG_WAITALL))
+    data = conn.recv(file_length, socket.MSG_WAITALL)
+    print filename
+    print file_length
+    file = open('new_' + filename, "wb")
+    file.write(data)
+    file.close()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.settimeout(360)
+s.listen(1)
+
+while True:
+    conn, addr = s.accept()
+    print 'Connection address:', addr
+
+    try:
+        command_length = ord(conn.recv(1))
+    except Exception:
+        continue
+
+    command = conn.recv(command_length, socket.MSG_WAITALL)
+
+    if command == UPLOAD_COMMAND:
+        tcp_download_file(conn)
+    elif command == DOWNLOAD_COMMAND:
+        filename_length = ord(conn.recv(1))
+        filename = conn.recv(filename_length, socket.MSG_WAITALL)
+        print filename
+        tcp_upload_file(conn, filename)
+
+    conn.close()
